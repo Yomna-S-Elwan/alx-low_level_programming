c2f54dde58177cf79a7cdca81ba58eef131d4d3b
Yomna-S-Elwan <Yomnasayedelwan@gmail.com>
2023-10-09 13:29:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-#include <unlstd.h>
+#include <unistd.h>
+
 /**
- * main - A c program that prints a line to the standard error
- *
- * Description: This function prints a line to the standard error.
- *
- * Return: 1:(Success)
- */
+* main - A c file to display a string to the standard err.
+*
+* Description: A c file to display a string to the standard err
+*
+* Return - 0 signifying program success
+*/
+
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
 return (1);
 }
