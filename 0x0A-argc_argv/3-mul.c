abf3e5cc3c31d3d38e02352b5bf10531e0c41d1f
Yomna-S-Elwan <Yomnasayedelwan@gmail.com>
2023-11-06 08:19:12 -0800

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main-print sum of 2 numbers
+ *main-print sum of 2 numbers.
  *@argc:number of command line arguments.
  *@argv:pointer to an array of command line arguments.
- *Return:0-success, non-zero-fail
+ *Return:0-success, non-zero-fail.
  */
 int main(int argc, char *argv[])
 {
